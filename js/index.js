8b9f33d4c1ea4672fea696b5ea5da74dacad22b7
VozzRoman <looper0500@ukr.net>
2022-11-27 10:57:06 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 
+const enterTask = document.querySelector('#newTask');
+const toDoList = document.querySelector('.todo-list');
+const buttonClear = document.querySelector('#clearList');
+console.log(enterTask);
+
+let STORAGE__KEY = 'to-do-list'
+
+enterTask.addEventListener('keydown', onPressEnter);
+function onPressEnter(e) {
+	if (e.key === 'Enter') {
+	const listDo = e.target.value;
+	const storageList = localStorage.setItem(STORAGE__KEY, listDo);
+		console.log(storageList);
+		backDate();
+	}
+
+}
+
+function backDate() {
+	const getStorageList = localStorage.getItem(STORAGE__KEY);
+	console.log(getStorageList);
+}
