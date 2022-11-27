05c12e00b2cc7fd6f860a1aaa34f464e38023249
EvaVozz <voznukeeva@ukr.net>
2022-11-27 15:53:05 +0200

    adding

--- js/index.js
+++ js/index.js
@@ -1,23 +1,51 @@
 
 const enterTask = document.querySelector('#newTask');
-const toDoList = document.querySelector('.todo-list');
+const toDoListContainer = document.querySelector('.todo-list');
 const buttonClear = document.querySelector('#clearList');
 console.log(enterTask);
 
-let STORAGE__KEY = 'to-do-list'
 
 enterTask.addEventListener('keydown', onPressEnter);
 function onPressEnter(e) {
-	if (e.key === 'Enter') {
-	const listDo = e.target.value;
-	const storageList = localStorage.setItem(STORAGE__KEY, listDo);
-		console.log(storageList);
-		backDate();
+if (e.key === 'Enter') {
+	let listDo = e.target.value;
+	let data = JSON.parse(localStorage.getItem('newList'));
+	console.log(data);
+	if (!data) {
+		data = [];
+	}
+		data.push(listDo);
+		console.log(data);
+		const dataToString = JSON.stringify(data);
+		localStorage.setItem('newList', dataToString);
+		e.target.value = '';
+
+
+
+		
+		// const storageList = localStorage.setItem(STORAGE__KEY, listDo);
+		
+		// console.log(storageList);
+		// toDoListContainer.append(makeToDoList());
+		
+
 	}
 
 }
 
-function backDate() {
+
+
+function makeToDoList() {
 	const getStorageList = localStorage.getItem(STORAGE__KEY);
-	console.log(getStorageList);
+	const list = document.createElement('li');
+	const span = document.createElement('span');
+	const buttonDone = document.createElement('button');
+	const buttonDel = document.createElement('button');
+	span.innerHTML = getStorageList;
+	buttonDone.textContent = 'Done';
+	buttonDel.textContent = 'Delete';
+	list.appendChild(span);
+	list.append(buttonDel);
+	list.append(buttonDone);
+	return list;
 }
